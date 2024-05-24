121a2c92084e179184edc4d40571ca359d5edacf
ParzivalFR <parzivaleu@gmail.com>
2024-05-24 11:10:14 +0200

    Fix Wavy Blue

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,3 +1,4 @@
+import Logo from "@/public/logo.png";
 import Image from "next/image";
 import BurgerMenu from "./BurgerMenu";
 
@@ -7,11 +8,20 @@ export default function Header({ src }) {
       <div className="w-full relative">
         <Image
           src={src}
-          alt="Logo Jazz en Barque"
+          alt="Image du festival Jazz en Barque"
           width={1920}
           height={1080}
-          className="w-full lg:h-svh object-cover"
+          className="w-full object-cover"
         />
+        <div className="absolute z-10 right-1/2 translate-x-1/2 bottom-1/2 translate-y-1/2">
+          <Image
+            src={Logo}
+            alt="Logo"
+            width={300}
+            height={300}
+            className="w-40 sm:w-60 lg:w-80"
+          />
+        </div>
       </div>
       <div className="absolute top-5 right-5">
         <BurgerMenu />
