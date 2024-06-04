64bd772835bab8f194fbb6dde9d36fb726a357ad
ParzivalFR <parzivaleu@gmail.com>
2024-06-04 08:30:15 +0200

    Fix size Text Address

--- app/components/Address.jsx
+++ app/components/Address.jsx
@@ -3,16 +3,16 @@ import Map from "./Map";
 const Address = () => {
   return (
     <section className="wave-purple flex flex-col items-center gap-6 w-svw h-auto bg-darkpurple py-10 px-4">
-      <div className="flex flex-col gap-8 px-4 pt-10">
-        <h3 className="text-white text-start text-3xl sm:text-4xl lg:text-8xl">
+      <div className="w-full md:max-w-[800px] flex flex-col gap-8 px-4 pt-10">
+        <h3 className="text-white text-start text-3xl sm:text-4xl ">
           Se rendre à l'évènement
         </h3>
-        <div className="flex flex-col gap-4 md:px-10 lg:px-20">
+        <div className="flex flex-col justify-start gap-4">
           <p className="text-white text-sm md:text-xl">
             Pour nous rejoindre, rien de plus simple, votre GPS saura nous
             trouver 🙂
           </p>
-          <p className="text-white flex text-sm md:text-xl">
+          <p className="text-white flex flex-col sm:flex-row text-sm md:text-xl">
             Notre adresse :{" "}
             <address>“Etang des landes” 41210 Saint Viâtre</address>
           </p>
--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -4,7 +4,7 @@ const Map = () => {
       src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10786.515155641167!2d1.9515154!3d47.4776628!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e4d5a934b87c17%3A0x13848450bbaf1979!2s%C3%89tang%20des%20Landes!5e0!3m2!1sfr!2sfr!4v1717480932585!5m2!1sfr!2sfr"
       width="300"
       height="200"
-      className="border-0 w-full md:max-w-[800px] md:h-72 xl:h-96"
+      className="border-0 w-full max-w-[800px] md:h-72 xl:h-96"
       allowfullscreen=""
       loading="lazy"
       referrerpolicy="no-referrer-when-downgrade"
