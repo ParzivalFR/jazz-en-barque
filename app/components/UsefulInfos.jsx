b0ee25c5b3ee8665f7c615dc025e642c96031c45
ParzivalFR <parzivaleu@gmail.com>
2024-06-08 08:42:54 +0200

    Delete icons and images

--- app/components/UsefulInfos.jsx
+++ app/components/UsefulInfos.jsx
@@ -1,59 +1,12 @@
 import Image from "next/image";
 import { londrina } from "../fonts";
-import Caravane from "../images/caravane.png";
-import Gîte from "../images/gîte.png";
-import Tente from "../images/tente.png";
+import DuckFoot from "../icons/duck-foot.svg";
+import MiddleMoon from "../icons/middle-moon.svg";
 import Address from "./Address";
 import Housing from "./Housing";
 import Spacing from "./Spacing";
-import MiddleMoon from "../icons/middle-moon.svg";
-import DuckFoot from "../icons/duck-foot.svg";
-import BigLeafBlue from "../icons/big-leaf-blue.svg";
 
 const UsefulInfos = () => {
-  const logements = [
-    {
-      icon: Gîte,
-      title: "Les gites à proximité",
-      description: [
-        {
-          text: "Pour ceux qui souhaitent dormir dans un lit douillet, il y a des gîtes à proximité du lieu de la fête.",
-          contact: "Vincent Henneckart - 06 66 78 86 36",
-        },
-        {
-          text: "N’hésitez pas à nous contacter pour plus d’informations.",
-        },
-        {
-          text: "Nous vous donnerons les coordonnées des gîtes les plus proches.",
-        },
-        {
-          text: "Attention, les places sont limitées, pensez à réserver à l’avance.",
-        },
-        {
-          text: "Pour ceux qui souhaitent dormir dans un lit douillet, il y a des gîtes à proximité du lieu de la fête.",
-        },
-      ],
-    },
-    {
-      icon: Caravane,
-      title: "Van, caravanne et camping car",
-      description:
-        "Pour les adeptes, vous pourrez trouver un petit coin sur place pour parker votre logement pour la nuit",
-    },
-    {
-      icon: Tente,
-      title: "Camping au bord de l’étang",
-      description: [
-        {
-          text: "Si vous avez une tente à monter, vous pouvez venir des 11h (prévoir le pique-nique).",
-        },
-        {
-          text: "Le lendemain matin n’hésitez pas à apporter ce qu’il vous faut pour prendre le petit-déjeuner tous ensemble.",
-        },
-      ],
-    },
-  ];
-
   return (
     <section className="wave-background flex flex-col items-center gap-6 w-full h-auto bg-background pt-10 px-4">
       <div className="flex flex-col gap-16 px-4 pt-10">
