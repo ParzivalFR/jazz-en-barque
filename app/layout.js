67c68b40bd4649b30749745cd17ab6e0c35a5c9f
ParzivalFR <parzivaleu@gmail.com>
2024-06-10 19:36:54 +0200

    Fix favicon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ export const metadata = {
   title: "Jazz En Barque - Embarquez pour une expérience musicale unique !",
   description:
     "Jazz En Barque est un festival unique en Sologne, célébrant la musique et l'art en plein air. Après une interruption en 2023, l'édition 2024 revient avec plus de passion et de créativité. Découvrez des artistes locaux, des sessions live et des animations inédites comme le concours de barques décorées et une grande course de barques. Profitez de moments magiques au bord de l'étang, avec des options d'hébergement pour tous les goûts, du camping aux charmantes maisons de campagne. ",
+  icons: {
+    favicon: "/favicon.ico",
+  },
 
   openGraph: {
     title: "Jazz En Barque - Embarquez pour une expérience musicale unique !",
